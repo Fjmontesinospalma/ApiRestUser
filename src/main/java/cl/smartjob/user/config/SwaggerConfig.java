9136cd73f2583731e8590767fb759e6003f63c77
Francisco Javier Montesinos Palma <extfmontesinos@transbank.cl>
2024-08-25 12:07:51 -0400

    Add Swagger

--- src/main/java/cl/smartjob/user/config/SwaggerConfig.java
+++ src/main/java/cl/smartjob/user/config/SwaggerConfig.java
@@ -1,12 +1,12 @@
 package cl.smartjob.user.config;
 
-import org.springdoc.core.GroupedOpenApi;
+
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
